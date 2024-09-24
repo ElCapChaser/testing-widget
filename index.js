9924130054590f579427735b1a63f07bb99c6eb2
stevenGYG <steven.verlinden@getyourguide.com>
2024-09-24 17:34:18 +0200

    Move everything to a function

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-console.log('getting loaded correctly')
-const button = document.getElementById('botting-label');
-const errorText = document.getElementById('error-text');
-console.log('button', button)
-button.addEventListener('click', function() {
-  // code to run when button is clicked
-  console.log('button clicked')
-  errorText.value = "Clicked"
-});
+(function() {
+    const myWidget = document.getElementById('my-widget');
+    console.log('widget',myWidget)
+    var iDiv = document.createElement('div');
+    iDiv.id = "input-box"
+    iDiv.class = "input-box"
+    myWidget.appendChild(iDiv)
+
+    const input = document.createElement('input');
+    input.id = "botting-label"
+    input.type = "checkbox"
+    myWidget.appendChild(input)
+
+    console.log('getting loaded correctly')
+    const button = document.getElementById('botting-label');
+    console.log('button', button)
+})();
